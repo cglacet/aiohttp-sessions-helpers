8f9b7ee0cca4b9189a23152a579289ecc04f1abb
Christian Glacet <christian.glacet@gmail.com>
2018-06-26 02:01:26 +0200

    Allow to pass the session object name as parameter for the decorator

--- asynctools/helpers.py
+++ asynctools/helpers.py
@@ -7,14 +7,14 @@ def attach_session(session_object_name=None):
     def decorator(method):
         @functools.wraps(method)
         async def method_with_session(self, *args, **kwargs):
-            if self.session is None:
+            if self._session is None:
                 async with aiohttp.ClientSession() as session:
                     #print("{} using a one shot session.".format(method.__name__))
-                    kwargs['session_object_name'] = session
+                    kwargs[session_object_name] = session
                     return await method(self, *args, **kwargs)
             else:
                 #print("{} using the long lasting session {}.".format(method.__name__, self.session))
-                kwargs[session_object_name] = self.session
+                kwargs[session_object_name] = self._session
                 return await method(self, *args, **kwargs)
         return method_with_session
     return decorator
@@ -22,17 +22,17 @@ def attach_session(session_object_name=None):
 class AbstractSessionContainer(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self, *args, **kwargs):
-        self.session = None
-        self.args = args
-        self.kwargs = kwargs
+        self._session = None
+        self._args = args
+        self._kwargs = kwargs
     async def __aenter__(self):
-        self.session = await aiohttp.ClientSession(*self.args, **self.kwargs).__aenter__()
+        self._session = await aiohttp.ClientSession(*self._args, **self._kwargs).__aenter__()
         return self
     async def __aexit__(self, *args, **kwargs):
-        await self.session.__aexit__(*args, **kwargs)
+        await self._session.__aexit__(*args, **kwargs)
     async def start_session(self, *args, **kwargs):
-        self.session = aiohttp.ClientSession(*args, **kwargs)
+        self._session = aiohttp.ClientSession(*args, **kwargs)
     async def close_session(self, *args, **kwargs):
-        if (self.session is not None) and (not self.session.closed):
-            await self.session.close()
-        self.session = None
+        if (self._session is not None) and (not self._session.closed):
+            await self._session.close()
+        self._session = None
